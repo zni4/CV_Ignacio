6d4f08a37424fc4ce6996695e5ce4712bc596dd4
zni4 <38941259+zni4@users.noreply.github.com>
2020-02-25 12:39:18 +0100

    Nueva versión CV por error al subir carpeta en el anterior

---
+++ resources/js/index.js
@@ -0,0 +1,117 @@
+﻿function enviarMail() {
+    document.location.href = "mailto:ignacio.nogueira@gmail.com";
+}
+
+function accederLinkedin() {
+    document.location.href = "https://linkedin.com/in/ignacionogueira";
+}
+
+function mostrar_Detalle(empresa) {
+    var divModal = document.getElementById('detalle');
+
+    var comentario = document.getElementById('detalle');
+
+    if (divModal != null) {
+        comentario.parentNode.removeChild(comentario);
+    }
+
+    //Obtenemos el contenedor e incluimos el elemento
+    var divNodo = document.getElementById('contenedor_detalle');
+
+    var texto = '';
+
+    var logo = '';
+    var años = '';
+
+    if (empresa === 'accenture') {
+        logo = 'logo_accenture.png'
+        años = TraducirClave('julio') + ' 1996 - ' + TraducirClave('julio') + ' 2002';
+        var texto = TraducirClave('accenture');
+    }
+    else if (empresa === 'travelsoft') {
+        logo = 'logo_travelsoft.png'
+        años = TraducirClave('diciembre') + ' 2002 - ' + TraducirClave('mayo') + ' 2003';
+        var texto = TraducirClave('travelsoft');
+    }
+    else if (empresa === 'amdocs') {
+        logo = 'logo_amdocs.png'
+        años = TraducirClave('diciembre') + ' 2003 - ' + TraducirClave('noviembre') + ' 2008';
+        var texto = TraducirClave('amdocs');
+    }
+    else if (empresa === 'geoban') {
+        logo = 'logo_Geoban.png'
+        años = TraducirClave('enero') + ' 2009 - ' + TraducirClave('junio') + ' 2009';
+        var texto = TraducirClave('geoban');
+    }
+    else if (empresa === 'vias') {
+        logo = 'logo_Vias.jpg'
+        años = TraducirClave('septiembre') + ' 2009 - ' + TraducirClave('octubre') + ' 2016';
+        var texto = TraducirClave('vias');
+    }
+    else if (empresa === 'dragados') {
+        logo = 'logo_Dragados.jpg'
+        d = new Date();
+
+        if (d.getMonth() === 0) {
+            mes = 'enero';
+        }
+        else if (d.getMonth() === 1) {
+            mes = 'febrero';
+        }
+        else if (d.getMonth() === 2) {
+            mes = 'marzo';
+        }
+        else if (d.getMonth() === 3) {
+            mes = 'abril';
+        }
+        else if (d.getMonth() === 4) {
+            mes = 'mayo';
+        }
+        else if (d.getMonth() === 5) {
+            mes = 'junio';
+        }
+        else if (d.getMonth() === 6) {
+            mes = 'julio';
+        }
+        else if (d.getMonth() === 7) {
+            mes = 'agosto';
+        }
+        else if (d.getMonth() === 8) {
+            mes = 'septiembre';
+        }
+        else if (d.getMonth() === 9) {
+            mes = 'octubre';
+        }
+        else if (d.getMonth() === 10) {
+            mes = 'noviembre';
+        }
+        else if (d.getMonth() === 11) {
+            mes = 'diciembre';
+        }
+
+        años = TraducirClave('octubre') + ' 2016 - ' + TraducirClave(mes) + ' ' + d.getFullYear();
+        var texto = TraducirClave('dragados');
+    }
+
+    divNodo.innerHTML +=
+        '<div id="detalle" class="detalle color_' + empresa + '"><div class="closeButton" onclick="ocultar_Detalle();">&times ' + TraducirClave("cerrar") + '<img class="logo_' + empresa +
+        '_detalle" src = "./resources/images/logos/' + logo + '" /></div><div class="año"><br>' +
+        años +
+        '</div><br><div class="texto_detalle">' + texto + '</div></div>';
+
+    divModal = divNodo;
+
+    document.getElementById('empresas').classList.add('invisible');
+}
+
+function ocultar_Detalle() {
+    var divModal = document.getElementById('detalle');
+
+    var comentario = document.getElementById('detalle');
+
+    if (divModal != null) {
+        comentario.parentNode.removeChild(comentario);
+    }
+
+    document.getElementById('empresas').classList.remove('invisible');
+}
---
+++ resources/js/tools.js
@@ -0,0 +1,155 @@
+﻿var mIdiomaActual;
+
+var mPaginaSinTraducir;
+
+function obtenerIdioma() {
+    var language;
+
+    if (window.navigator.languages) {
+        language = window.navigator.languages[0];
+    } else {
+        language = window.navigator.userLanguage || window.navigator.language;
+
+        language = language.substr(0, language.indexOf('-'));
+    }
+
+    mIdiomaActual = language.toUpperCase();
+}
+
+function informarContenidoSinTraducir() {
+    if (mPaginaSinTraducir === undefined && document.body.innerHTML !== undefined) {
+        mPaginaSinTraducir = document.body.innerHTML
+    }
+}
+
+function seleccionarIdioma(idioma) {
+    var paginaSinTraducir = mPaginaSinTraducir;
+    TraducirPagina(paginaSinTraducir, idioma);
+
+    if (idioma === 'ES') {
+        document.getElementById('idiomaEspañolSeleccionado').style.opacity = "1";
+        document.getElementById('idiomaInglesSeleccionado').style.opacity = "0.5";
+
+    }
+    else if (idioma === 'EN' || idioma === undefined) {
+        document.getElementById('idiomaEspañolSeleccionado').style.opacity = "0.5";
+        document.getElementById('idiomaInglesSeleccionado').style.opacity = "1";
+    }
+
+    mIdiomaActual = idioma;
+}
+
+/*
+function TraducirLiteral(literal, idioma) {
+    if (literal != null) {
+        if (idioma == undefined) {
+            idioma = mIdiomaActual;
+        }
+        if (idioma == null || idioma == "") { idioma = 'ES' };
+        if (literal.indexOf('<' + idioma + '>') != -1) {
+            inicio = literal.indexOf('<' + idioma + '>');
+            inicio += ('<' + idioma + '>').length
+            fin = literal.lastIndexOf('</' + idioma + '>');
+            valorOriginal = literal.substring(inicio, fin);
+            return valorOriginal
+        }
+        else {
+            return literal
+        }
+    }
+    else {
+        return ''
+    }
+}
+*/
+
+function TraducirClave(clave, idioma) {
+
+    if (idioma == undefined) {
+        idioma = mIdiomaActual;
+    }
+
+    var rutaLiterales = './resources/Textos.xml';
+
+    var textoTraducido = "";
+
+    //Obtener la traducción de la clave
+    var xmlDoc = AbrirFichero(rutaLiterales);
+
+    for (j = 0; j < xmlDoc.getElementsByTagName('Literal').length; j++) {
+        if (clave === xmlDoc.getElementsByTagName('Literal')[j].getAttribute('Id')) {
+            textoTraducido = xmlDoc.getElementsByTagName(idioma)[j].childNodes[0].nodeValue;
+            j = xmlDoc.getElementsByTagName('Literal').length; //Para salir del bucle
+        }
+    }
+
+    textoTraducido = formatear_Texto(textoTraducido);
+
+    return textoTraducido;
+}
+
+function formatear_Texto(texto) {
+    texto = texto.replace(/'@bold'/g, '<b>');
+    texto = texto.replace(/'bold@'/g, '</b>');
+    texto = texto.replace(/'@newline'/g, '<br>');
+
+    return texto
+}
+
+function TraducirPagina(paginaSinTraducir, idioma) {
+    var contenido = paginaSinTraducir;
+
+    var rutaLiterales = './resources/Textos.xml';
+
+    let numeroTraducciones = contenido.match(new RegExp('{', 'g')).length;
+
+    var textoTraducido = "";
+
+    //Obtener la traducción de la clave
+    var xmlDoc = AbrirFichero(rutaLiterales);
+
+    for (i = 0; i < numeroTraducciones; i++) {
+
+        let posicioninicial = contenido.indexOf('{') + 1;
+        let posicionfinal = contenido.indexOf('}');
+        let clave = contenido.substr(posicioninicial, (posicionfinal - posicioninicial));
+
+        for (j = 0; j < xmlDoc.getElementsByTagName('Literal').length; j++) {
+            if (clave === xmlDoc.getElementsByTagName('Literal')[j].getAttribute('Id')) {
+                textoTraducido = xmlDoc.getElementsByTagName(idioma)[j].childNodes[0].nodeValue;
+                j = xmlDoc.getElementsByTagName('Literal').length; //Para salir del bucle
+            }
+        }
+
+        textoTraducido = formatear_Texto(textoTraducido);
+
+        contenido = contenido.replace("{" + clave + "}", textoTraducido);
+
+        document.body.innerHTML = contenido;
+    }
+}
+
+function AbrirFichero(fichXML) {
+    var xmlDoc = undefined;
+    try {
+        if (document.all) {
+            xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+        } else {
+            xmlDoc = document.implementation.createDocument("", "", null);
+        }
+        xmlDoc.async = false;
+        xmlDoc.load(fichXML);
+    } catch (e) {
+        try { //otros safari, chrome
+            var xmlhttp = new window.XMLHttpRequest();
+            xmlhttp.open("GET", fichXML, false);
+            xmlhttp.send(null);
+            xmlDoc = xmlhttp.responseXML.documentElement;
+            return xmlDoc;
+        }
+        catch (e) {
+            return undefined;
+        }
+    }
+    return xmlDoc;
+}
