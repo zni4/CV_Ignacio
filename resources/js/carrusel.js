6cf8f9656f8be0d0851b89fcb375517f5921f4ef
zni4 <zni_4@hotmail.com>
2020-02-28 22:57:38 +0100

    Carrusel conocimientos arreglado

--- resources/js/carrusel.js
+++ resources/js/carrusel.js
@@ -1,56 +1,69 @@
 var slideIndex = 1;
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+	showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+	showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("tarjeta_conocimiento");
-    var dots = document.getElementsByClassName("punto");
+	var i;
+	var slides = document.getElementsByClassName('tarjeta_conocimiento');
+	var dots = document.getElementsByClassName('punto');
 
-    if (n > slides.length) {
-        slideIndex = 1
-    }
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
 
-    if (n < 1) {
-        slideIndex = slides.length
-    }
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none';
+		slides[i].classList.remove('notfocus');
+		slides[i].classList.remove('anterior');
+		slides[i].classList.remove('siguiente');
+	}
 
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" activo", "");
-    }
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(' activo', '');
+	}
 
-    slides[slideIndex - 1].style.display = "block";
-    slides[slideIndex - 1].classList.remove("notfocus");//TEST
+	slides[slideIndex - 1].style.display = 'block';
+	slides[slideIndex - 1].classList.remove('notfocus');
+	slides[slideIndex - 1].classList.remove('anterior');
+	slides[slideIndex - 1].classList.remove('siguiente');
 
-    dots[slideIndex - 1].className += " activo";
+	dots[slideIndex - 1].className += ' activo';
 
-    //TEST-INICIO
-    var anterior = slideIndex - 2;
-    var siguiente = slideIndex;
+	var anterior = slideIndex - 2;
+	var siguiente = slideIndex;
 
-    if (n == slides.length) {
-        anterior = slides.length - 2;
-        siguiente = 0;
-    }
-    if (n == 1) {
-        anterior = slides.length - 1;
-        siguiente = slideIndex;
-    }
+	if (n == slides.length) {
+		anterior = slides.length - 2;
+		siguiente = 0;
+	}
+	if (n == 1) {
+		anterior = slides.length - 1;
+		siguiente = slideIndex;
+	}
 
-    slides[anterior].style.display = "block";
-    slides[anterior].className += " notfocus";
+	if (n < 1) {
+		anterior = slides.length - 2;
+		siguiente = n;
+	}
 
-    slides[siguiente].style.display = "block";
-    slides[siguiente].className += " notfocus";
-    //TEST-FIN
+	if (n > slides.length) {
+		anterior = slides.length - 1;
+		siguiente = 1;
+	}
+
+	slides[anterior].style.display = 'block';
+	slides[anterior].className += ' notfocus anterior';
+
+	slides[siguiente].style.display = 'block';
+	slides[siguiente].className += ' notfocus siguiente';
 }
